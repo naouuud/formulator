b6c8f4be9c79314705da57f37b6c7b0415f2e556
nour <naoude@gmail.com>
2026-01-01 23:30:48 +0200

    Drag and drop to reorder/create group

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,19 +1,15 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { uiTypes } from '../models/ui-types';
 import { UiTypeIcon } from '../ui-type-icon/ui-type-icon';
-import { CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
-import { BuilderModel } from '../services/builder-model';
+import { CdkDrag, CdkDragPlaceholder, CdkDropList } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [UiTypeIcon, CdkDropList, CommonModule],
+  imports: [UiTypeIcon, CdkDropList, CdkDrag, CdkDragPlaceholder, CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
   uiTypes = uiTypes;
-  // @ViewChild('builderList', { static: true }) builderList!: CdkDropList;
-
-  constructor(private builderModel: BuilderModel) {}
 }
--- src/app/ui-type-icon/ui-type-icon.ts
+++ src/app/ui-type-icon/ui-type-icon.ts
@@ -1,10 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { UiType } from '../models/ui-types';
-import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-ui-type-icon',
-  imports: [CdkDrag],
   templateUrl: './ui-type-icon.html',
   styleUrl: './ui-type-icon.css',
 })
