8747cea38aed9c043be79f64ae45f679a416799b
nour <naoude@gmail.com>
2026-01-08 20:29:45 +0200

    Wiring props to service - persist changes

--- src/app/builder/builder-field-text/builder-field-text.ts
+++ src/app/builder/builder-field-text/builder-field-text.ts
@@ -1,11 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Field } from '../../models/field-types';
-import { PropType } from '../../models/prop-types';
+import { Prop, PropChangeEvent, PropType, PropValueMap } from '../../models/prop-types';
+import { BuilderService } from '../../services/builder-service';
+import { throttleTime } from 'rxjs';
+import { BuilderPropLabel } from '../builder-prop-label/builder-prop-label';
 
 @Component({
   selector: 'app-builder-field-text',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, BuilderPropLabel],
   templateUrl: './builder-field-text.html',
   styleUrl: './builder-field-text.css',
 })
@@ -16,23 +25,43 @@ export class BuilderFieldText implements OnInit {
   array = Array.from({ length: 101 }, (_, i) => i);
   isValidationOpen = true;
 
+  constructor(private builderService: BuilderService) {}
+
   ngOnInit(): void {
-    this.formGroup = new FormGroup({
-      label: new FormControl(this.field.getPropValue(PropType.LABEL) ?? '', {
-        nonNullable: true,
-        validators: [Validators.maxLength(100)],
-      }),
-      placeholder: new FormControl(this.field.getPropValue(PropType.PLACEHOLDER) ?? '', {
-        nonNullable: true,
-        validators: [Validators.maxLength(100)],
-      }),
-      required: new FormControl(this.field.getPropValue(PropType.REQUIRED), { nonNullable: true }),
-      maxlength: new FormControl(this.field.getPropValue(PropType.MAXLENGTH), {
-        nonNullable: true,
-      }),
-      minlength: new FormControl(this.field.getPropValue(PropType.MINLENGTH), {
-        nonNullable: true,
-      }),
+    // this.formGroup = new FormGroup({
+    //   label: new FormControl(this.field.getPropValue(PropType.LABEL) ?? '', {
+    //     nonNullable: true,
+    //     validators: [Validators.maxLength(100)],
+    //   }),
+    //   placeholder: new FormControl(this.field.getPropValue(PropType.PLACEHOLDER) ?? '', {
+    //     nonNullable: true,
+    //     validators: [Validators.maxLength(100)],
+    //   }),
+    //   required: new FormControl(this.field.getPropValue(PropType.REQUIRED), { nonNullable: true }),
+    //   maxlength: new FormControl(this.field.getPropValue(PropType.MAXLENGTH), {
+    //     nonNullable: true,
+    //   }),
+    //   minlength: new FormControl(this.field.getPropValue(PropType.MINLENGTH), {
+    //     nonNullable: true,
+    //   }),
+    // });
+    this._buildFormGroup();
+  }
+
+  setProp_C(valueIn: PropChangeEvent) {
+    const { propType, value } = valueIn;
+    this.builderService.setProp_S(this.field, propType, value);
+  }
+
+  private _buildFormGroup(): void {
+    this.field.props.forEach((prop: Prop) => {
+      const validators: ValidatorFn[] = [];
+      switch (prop.propType) {
+        case PropType.LABEL:
+          validators.push(Validators.maxLength(100));
+      }
+      const control = new FormControl(prop.value, { nonNullable: true, validators });
+      this.formGroup.addControl(prop.propType, control);
     });
   }
 }
