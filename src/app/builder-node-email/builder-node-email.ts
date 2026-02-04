b1081d8ef104c63e158621a704785a828157025b
nour <naoude@gmail.com>
2026-02-04 17:47:38 +0200

    Small changes

---
+++ src/app/builder-node-email/builder-node-email.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { BuilderPropLabel } from '../builder/builder-prop-label/builder-prop-label';
+import { BuilderValidation } from '../builder/builder-validation/builder-validation';
+import { Node } from '../models/node';
+
+@Component({
+  selector: 'app-builder-node-email',
+  imports: [BuilderPropLabel, BuilderValidation],
+  templateUrl: './builder-node-email.html',
+  styleUrl: './builder-node-email.css',
+})
+export class BuilderNodeEmail {
+  @Input() node!: Node;
+}
---
+++ src/app/builder-node-phone/builder-node-phone.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { BuilderPropLabel } from '../builder/builder-prop-label/builder-prop-label';
+import { BuilderValidation } from '../builder/builder-validation/builder-validation';
+import { Node } from '../models/node';
+
+@Component({
+  selector: 'app-builder-node-phone',
+  imports: [BuilderPropLabel, BuilderValidation],
+  templateUrl: './builder-node-phone.html',
+  styleUrl: './builder-node-phone.css',
+})
+export class BuilderNodePhone {
+  @Input() node!: Node;
+}
